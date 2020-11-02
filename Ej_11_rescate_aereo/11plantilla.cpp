170df489920ddc1267aeab638be1bb3056f27012
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2020-11-02 19:40:56 +0100

    Exercice 11 donde, and correct

--- Ej_11_rescate_aereo/11plantilla.cpp
+++ Ej_11_rescate_aereo/11plantilla.cpp
@@ -17,12 +17,27 @@
 
 // La función recibe un vector con las alturas de los edificios, y la altura de la nave que realizará el rescate
 // Devuelve las posiciones de inicio y fin del segmento máximo encontrado.
-void resolver(std::vector<int> const& v, int alturaNave, int & posIni, int& posFin) 
-
+void resolver(std::vector<int> const& v, int alturaNave, int & posIni, int& posFin){
+    int conSec = 0;  //Contador de la secuencia
+    int secMax = 0;
 // Codigo del alumno
-
-
-
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (v[i] > alturaNave) //Continua la racha
+        {
+            ++conSec;
+            if (conSec > secMax)//Mejora la racha anterior, NO iguala, ya que nos quedamos con la que esté más a la izq
+            {
+                secMax = conSec;
+                posIni = i - conSec + 1;
+                posFin = i;
+            }
+            // Si iguala la racha -> no hacemos nada, porque nos quedamos con la anterior
+        }
+        else { //Fin de la racha actual
+            conSec = 0;
+        }
+    }
 }
 
 // Funcion que lee de la entrada, 
@@ -35,17 +50,17 @@ void resuelveCaso() {
     std::vector<int> v(numElem);
     for (int& i : v) std::cin >> i;
     // LLamada a la función resolver
-    int p, q;
-    resolver(v, alturaNave, p, q);
+    int i, f; //Inicio y final del intervalo
+    resolver(v, alturaNave, i, f);
     // Escribir el resultado
-    std::cout << p << ' ' << q << "\n";
+    std::cout << i << ' ' << f << "\n";
 }
 
 int main() {
     // Para la entrada por fichero.
     // Comentar para acepta el reto
 #ifndef DOMJUDGE
-    std::ifstream in("datos.txt");
+    std::ifstream in("1.in");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif
 
