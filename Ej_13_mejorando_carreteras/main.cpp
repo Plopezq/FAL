31e8fb285d59910a9f00f1d0a29fec92babf202f
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2020-11-04 10:39:19 +0100

    Exercise 13 done and correct

--- Ej_13_mejorando_carreteras/main.cpp
+++ Ej_13_mejorando_carreteras/main.cpp
@@ -5,25 +5,54 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <vector>
+#include<algorithm>
+using namespace std;
 
 
 // función que resuelve el problema
-TipoSolucion resolver(TipoDatos datos) {
-
-
+int resolver(vector<int> const& vector) {
+    //Se busca el tramo que mas accidentes sufre. 
+    //Como tenemos el vector odenado, tan solo es buscar el segmento máximo de valores iguales.
+    int lonMax = 0;
+    int lonAct = 0;
+    int posRa = 0; //Ultimo punto donde hubo racha.
+    for (int i = 1; i < vector.size(); i++) {
+        if (vector[i] == vector[i-1]) {//Continua la racha
+            ++lonAct;
+            if (lonAct > lonMax) {//Mejora la racha
+                lonMax = lonAct;
+                posRa = i;
+            } //Si iguala la racha, no hacemos nada, ya que nos quedamos con ls de la izq
+        }
+        else {//Se rompe la racha
+            lonAct = 0;
+        }
+    }
+    return (posRa-lonMax);
 }
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
     // leer los datos de la entrada
-
-    if (caso especial)
+    int numAcci;
+    cin >> numAcci;
+    if (numAcci == -1)
         return false;
 
-    TipoSolucion sol = resolver(datos);
+    vector<int> accidentes;
+    for (int i = 0; i < numAcci; i++) {
+        int aux;
+        cin >> aux;
+        accidentes.push_back(aux);
+    }
+    sort(accidentes.begin(), accidentes.end());
 
-    // escribir sol
+    int sol = resolver(accidentes);
+
+    //Escribir solucion
+    cout << accidentes[sol] << endl;
 
     return true;
 
@@ -33,7 +62,7 @@ int main() {
     // Para la entrada por fichero.
     // Comentar para acepta el reto
 #ifndef DOMJUDGE
-    std::ifstream in("datos.txt");
+    std::ifstream in("1.in");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
 #endif 
 
