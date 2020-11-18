23567523566dc6d332fc832b4bc52fd71bd9ce47
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2020-11-18 19:56:42 +0100

    Exercise 25 done and correct

--- ej_25_digitos_complementarios/main.cpp
+++ ej_25_digitos_complementarios/main.cpp
@@ -8,27 +8,47 @@
 
 using namespace std;
 using lli = long long int;
-
+struct tSol {
+    int num, pot;
+};
 // función recursiva NO final que calcula el numero con los digitos complementarios
-lli rec1(lli const numero) { //Done
+lli rec1NOfin(lli const numero) { //Caso 1 clase
     //Caso base
     if (numero < 10) {
         return abs(9 - (numero % 10));
     }//Caso recursivo
-    else {
-        return rec1(numero / 10) * 10 + abs(9 - (numero % 10));
+    else { //n >= 10
+        return rec1NOfin(numero / 10) * 10 + (9 - numero % 10);
+    }
+}
+lli rec1fin(lli const numero, lli acum, lli pot) { //Caclulamos el numero complementario del dado -- Caso 2 clase
+    //Caso base
+    if (numero < 10) {
+        return (9 - numero % 10) * pot + acum;
+    }
+    else {//Caso recursivo
+        return rec1fin(numero / 10, (9 - numero % 10) * pot + acum, pot * 10);
     }
 }
 
-//Funcion recursiva NO final que saca el complementario e invierte el numero 
-lli rec2(lli const numero, lli const acum) { //Falla con los 0
-
+//Funcion recursiva final que saca el complementario e invierte el numero 
+lli rec2fin(lli const numero, lli const acum) { //Falla con los 0 -- Caso 3 clase
     //Caso base
-    if (numero == 0) {
-        return acum;
+    if (numero < 10 ) {
+        return acum * 10 + (9 - numero);
     }//Caso recursivo
+    else { // n >= 10
+        return rec2fin(numero/10, acum * 10 + (9 - numero % 10));
+    }
+}
+tSol rec2NOfin(int const numero) { //Falla con los 0 -- Caso 4 clase
+
+    if (numero < 10) {
+        return { 9 - numero, 10 };
+    }
     else {
-        return rec2(numero / 10, acum * 10 + abs(9 - (numero % 10)));
+        tSol s = rec2NOfin(numero / 10);
+        return{ s.num + (9 - numero % 10) * s.pot, s.pot * 10 };
     }
 }
 
@@ -39,10 +59,10 @@ void resuelveCaso() {
     // leer los datos de la entrada
     lli numero = -1;
     cin >> numero;
-    lli num1 = rec1(numero);
-    lli num2 = rec2(numero, 0);
+    lli num1 = rec1fin(numero, 0, 1);
+    lli num2 = rec2fin(numero, 0);
     // escribir sol
-    cout  << num1 << " " << num2  << endl;
+    cout  << num1 << " " << num2 << endl;
 }
 
 int main() {
