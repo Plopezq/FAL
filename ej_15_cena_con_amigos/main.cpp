adc5844e3afc807f995f06c77db7f7964a1a3fff
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2020-11-23 22:41:49 +0100

    Exercise 15 done and correct

--- ej_15_cena_con_amigos/main.cpp
+++ ej_15_cena_con_amigos/main.cpp
@@ -12,24 +12,30 @@ using namespace std;
 
 // función que resuelve el problema
 void resolver(int &sumaMax, int &diaIni, int &lonMax, vector<int> const& secuencia) {
-
+    /* En este caso llevaremos en una variable la suma maxima encontrada hasta el momento en que estamos ejecutando
+    y en otra la suma del ultimo segmento que estamos tratando. 
+    El segmento actual acaba si la suma se hace negativa ya que en este caso conviene 
+    comenzar un nuevo segmento en lugar de acumular al segmento anterior.
+    */
     int sumRacha = 0;
     int lonRacha = 0;
     for (int i = 0; i < secuencia.size(); i++) { //Bucle que recorre la secuencia de dias
         sumRacha += secuencia[i];
         lonRacha++;
-        if (sumRacha > sumaMax) { //Nueva racha maxima
-            lonMax = lonRacha;
-            sumaMax = sumRacha;
-            diaIni = i - lonRacha;
-        }
-        else if (sumRacha == sumaMax) { //Iguala a la racha anterior
-            if (lonRacha < lonMax) { //Nos quedamos con la que tenga menor longitd
+        if (sumRacha > 0) { //La racha es positiva, seguimos
+            if (sumRacha > sumaMax) { //Tenemos nueva racha maxima
                 lonMax = lonRacha;
-                diaIni = i - lonRacha;
+                sumaMax = sumRacha;
+                diaIni = i - lonRacha + 1;
+            }else if (sumRacha == sumaMax) { //Iguala a la racha anterior
+                if (lonRacha < lonMax) { //Nos quedamos con la que tenga menor longitd
+                    lonMax = lonRacha;
+                    diaIni = i - lonRacha + 1;
+                }
             }
+
         }
-        else {//Se rompe la racha
+        else { //Se rompe la racha, la suma se hace negativa y conviene comenzar otro segmento
             sumRacha = 0;
             lonRacha = 0;
         }
@@ -59,7 +65,7 @@ bool resuelveCaso() {
 
     // escribir sol
     cout << sumaMax << " " << diaIni << " " << lonMax << endl;
-    return true;
+   return true;
 
 }
 
