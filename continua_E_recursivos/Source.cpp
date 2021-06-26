c23c64478aee07a2cdca4cab7ea05539f02c4a05
Pablo Lopez Martin <lopezmartinpablo@gmail.com>
2021-06-26 13:25:04 +0200

    Mas ejercicios

---
+++ continua_E_recursivos/Source.cpp
@@ -0,0 +1,61 @@
+﻿// Nombre del alumno: Pablo Lopez Martin
+// Usuario del Juez: E-40
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+// función que resuelve el problema
+int resolver(int numero) {
+    if (numero < 10) {//caso base
+        return numero;//es el primer digito
+    }
+    else {//caso recursivo
+        if ((numero % 100 / 10) > numero % 10) {//lo sacamos
+            return resolver(numero / 10) * 10 + (numero % 10);
+        }
+        else {// NO lo sacamos
+            return resolver(numero / 10);
+        }
+    }
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    // leer los datos de la entrada
+    int numero;
+    cin >> numero;
+
+    int sol = resolver(numero);
+    // escribir sol
+    cout << sol << endl;
+
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("datos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
